4be054192801b3e0f0c316e53cc9308d71542843
daihaoxin <daihaoxin@126.com>
2019-03-24 15:30:16 +0800

    Updates

---
+++ precache-manifest.2b1cd9eeab8fbd6e11f8ce71c9ab40aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed713bf21a588f9f157b",
+    "url": "/accounts-app/static/js/runtime~main.dcd08200.js"
+  },
+  {
+    "revision": "ee2b464cf197daf2e4dc",
+    "url": "/accounts-app/static/js/main.653d8326.chunk.js"
+  },
+  {
+    "revision": "c915f4e7d66edb98ef49",
+    "url": "/accounts-app/static/js/2.9a3acd3a.chunk.js"
+  },
+  {
+    "revision": "ee2b464cf197daf2e4dc",
+    "url": "/accounts-app/static/css/main.aa67924b.chunk.css"
+  },
+  {
+    "revision": "83ffd68575e538d8348f52bd576bbfa7",
+    "url": "/accounts-app/index.html"
+  }
+];
